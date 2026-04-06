bc11b8319a7ebb8d00d594c09ef8cb626bef84b4
James H <jamesheff13@gmail.com>
2026-04-06 13:43:35 -0400

    Changes made

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,17 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "James"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println("More cups!");
         // STUDENT_TODO_2B: Add a second brew-related improvement
+        cups++;
+        System.out.println("Even more cups!");
     }
 
     public static void printSummary() {
